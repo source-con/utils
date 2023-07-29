fad86db9f9bf27dd97c9deb0cd3457c418e4a66d
Swapnil Nakade <46112765+swagftw@users.noreply.github.com>
2023-07-29 19:15:27 +0530

    refactor: context assignment and add new middlewares.
    This commit refactors the context assignment in the transaction method simplifying it into a single line, aiming to improve the readability of the code.
    The middleware.go file has been extended with additional functions to handle Binding Errors and Basic Authorization.
    These updates aim at offering a better handling of errors and improving security by requiring authentication, using SHA512 for comparing username and password.

--- database/transaction/postgres/tx.go
+++ database/transaction/postgres/tx.go
@@ -1,26 +1,24 @@
 package postgres
 
 import (
-    "context"
+	"context"
 
-    "gorm.io/gorm"
+	"gorm.io/gorm"
 
-    "github.com/source-con/utils/database/transaction"
-    "github.com/source-con/utils/types"
+	"github.com/source-con/utils/database/transaction"
+	"github.com/source-con/utils/types"
 )
 
 type PgTx struct {
-    pgConn *gorm.DB
+	pgConn *gorm.DB
 }
 
 func (p PgTx) Transaction(ctx context.Context, f func(ctx context.Context) error) error {
-    return p.pgConn.Transaction(func(tx *gorm.DB) error {
-        ctx = context.WithValue(ctx, types.TxCtxKey, tx)
-
-        return f(ctx)
-    })
+	return p.pgConn.Transaction(func(tx *gorm.DB) error {
+		return f(context.WithValue(ctx, types.TxCtxKey, tx))
+	})
 }
 
 func NewTx(pgConn *gorm.DB) transaction.Tx {
-    return &PgTx{pgConn: pgConn}
+	return &PgTx{pgConn: pgConn}
 }
--- serverutils/middleware.go
+++ serverutils/middleware.go
@@ -1,189 +1,235 @@
 package serverutils
 
 import (
-    "context"
-    "net/http"
-    "strings"
-    "time"
+	"bytes"
+	"context"
+	"crypto/sha512"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 
-    "github.com/gin-gonic/gin"
-    "github.com/go-playground/validator/v10"
-    jwtv5 "github.com/golang-jwt/jwt/v5"
-    "github.com/hashicorp/go-uuid"
-    pkgerr "github.com/pkg/errors"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	jwtv5 "github.com/golang-jwt/jwt/v5"
+	"github.com/hashicorp/go-uuid"
+	pkgerr "github.com/pkg/errors"
 
-    "github.com/source-con/utils"
-    "github.com/source-con/utils/errors"
-    "github.com/source-con/utils/logger"
-    "github.com/source-con/utils/types"
+	"github.com/source-con/utils"
+	"github.com/source-con/utils/errors"
+	"github.com/source-con/utils/logger"
+	"github.com/source-con/utils/types"
 )
 
 // LatencyLoggerMiddleware is Gin middleware which logs the latency of the request
 func LatencyLoggerMiddleware() gin.HandlerFunc {
-    log := logger.GetInstance()
+	log := logger.GetInstance()
 
-    return func(c *gin.Context) {
-        start := time.Now()
+	return func(c *gin.Context) {
+		start := time.Now()
 
-        c.Next()
+		c.Next()
 
-        latency := time.Since(start)
+		latency := time.Since(start)
 
-        log.Debug(context.Background(), "response latency", map[string]interface{}{
-            "latency":   latency,
-            "requestID": c.GetString(string(types.RequestIDCtxKey)),
-            "method":    c.Request.Method,
-            "path":      c.Request.URL.Path,
-        })
-    }
+		log.Debug(context.Background(), "response latency", map[string]interface{}{
+			"latency":   latency,
+			"requestID": c.GetString(string(types.RequestIDCtxKey)),
+			"method":    c.Request.Method,
+			"path":      c.Request.URL.Path,
+		})
+	}
 }
 
 // RequestIDMiddleware is Gin middleware which generates a requestID and sets it in the context
 func RequestIDMiddleware() gin.HandlerFunc {
-    log := logger.GetInstance()
+	log := logger.GetInstance()
 
-    return func(c *gin.Context) {
-        requestID, err := uuid.GenerateUUID()
-        if err != nil {
-            log.Error(context.Background(), err, "failed to generate requestID", nil)
-        }
+	return func(c *gin.Context) {
+		requestID, err := uuid.GenerateUUID()
+		if err != nil {
+			log.Error(context.Background(), err, "failed to generate requestID", nil)
+		}
 
-        c.Header("request-id", requestID)
-        c.Set(string(types.RequestIDCtxKey), requestID)
+		c.Header("request-id", requestID)
+		c.Set(string(types.RequestIDCtxKey), requestID)
 
-        c.Next()
-    }
+		c.Next()
+	}
 }
 
 // AuthMiddleware is a Gin middleware to authenticate the user using JWT
 func AuthMiddleware(secret string, claimKeys ...string) gin.HandlerFunc {
-    return func(c *gin.Context) {
-        // get accessToken from context
-        accessToken := c.GetHeader("Authorization")
-        if accessToken == "" {
-            c.AbortWithStatusJSON(
-                http.StatusUnauthorized,
-                errors.HTTPError{
-                    Code:       http.StatusUnauthorized,
-                    Message:    "empty access token",
-                    Err:        "empty access token provided",
-                    Resolution: "login again / provide access token",
-                    Meta:       nil,
-                },
-            )
+	return func(c *gin.Context) {
+		// get accessToken from context
+		accessToken := c.GetHeader("Authorization")
+		if accessToken == "" {
+			c.AbortWithStatusJSON(
+				http.StatusUnauthorized,
+				errors.HTTPError{
+					Code:       http.StatusUnauthorized,
+					Message:    "empty access token",
+					Err:        "empty access token provided",
+					Resolution: "login again / provide access token",
+					Meta:       nil,
+				},
+			)
 
-            return
-        }
+			return
+		}
 
-        // split accessToken in 2 parts and check if first part is "Bearer"
-        parts := strings.Split(accessToken, " ")
-        if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
-            c.AbortWithStatusJSON(
-                http.StatusUnauthorized,
-                errors.HTTPError{
-                    Code:       http.StatusUnauthorized,
-                    Message:    "invalid access token",
-                    Err:        "invalid access token provided",
-                    Resolution: "login again / provide valid access token",
-                    Meta:       nil,
-                })
+		// split accessToken in 2 parts and check if first part is "Bearer"
+		parts := strings.Split(accessToken, " ")
+		if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+			c.AbortWithStatusJSON(
+				http.StatusUnauthorized,
+				errors.HTTPError{
+					Code:       http.StatusUnauthorized,
+					Message:    "invalid access token",
+					Err:        "invalid access token provided",
+					Resolution: "login again / provide valid access token",
+					Meta:       nil,
+				})
 
-            return
-        }
+			return
+		}
 
-        tokenString := parts[1]
+		tokenString := parts[1]
 
-        // parse token
-        token, err := utils.ParseToken(tokenString, secret)
-        if err != nil {
-            c.AbortWithStatusJSON(http.StatusUnauthorized,
-                errors.HTTPError{
-                    Code:       http.StatusUnauthorized,
-                    Message:    "invalid access token",
-                    Err:        err.Error(),
-                    Resolution: "login again / provide valid access token",
-                    Meta:       nil,
-                })
+		// parse token
+		token, err := utils.ParseToken(tokenString, secret)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized,
+				errors.HTTPError{
+					Code:       http.StatusUnauthorized,
+					Message:    "invalid access token",
+					Err:        err.Error(),
+					Resolution: "login again / provide valid access token",
+					Meta:       nil,
+				})
 
-            return
-        }
+			return
+		}
 
-        mapClaims, ok := token.Claims.(jwtv5.MapClaims)
-        if !ok {
-            c.AbortWithStatusJSON(http.StatusUnauthorized,
-                errors.HTTPError{
-                    Code:       http.StatusUnauthorized,
-                    Message:    errors.ISE,
-                    Err:        "failed to parse claimKeys",
-                    Resolution: errors.TAL,
-                    Meta:       nil,
-                })
+		mapClaims, ok := token.Claims.(jwtv5.MapClaims)
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusUnauthorized,
+				errors.HTTPError{
+					Code:       http.StatusUnauthorized,
+					Message:    errors.ISE,
+					Err:        "failed to parse claimKeys",
+					Resolution: errors.TAL,
+					Meta:       nil,
+				})
 
-            return
-        }
+			return
+		}
 
-        userRole := mapClaims["role"]
-        if userRole == nil || userRole == "unknown" {
-            c.AbortWithStatusJSON(http.StatusForbidden, errors.HTTPError{
-                Code:       http.StatusForbidden,
-                Message:    "access forbidden: unknown user role",
-                Err:        "unknown user role",
-                Resolution: "login again",
-                Meta:       nil,
-            })
+		userRole := mapClaims["role"]
+		if userRole == nil || userRole == "unknown" {
+			c.AbortWithStatusJSON(http.StatusForbidden, errors.HTTPError{
+				Code:       http.StatusForbidden,
+				Message:    "access forbidden: unknown user role",
+				Err:        "unknown user role",
+				Resolution: "login again",
+				Meta:       nil,
+			})
 
-            return
-        }
+			return
+		}
 
-        for _, claim := range claimKeys {
-            c.Set(claim, mapClaims[claim])
-        }
+		for _, claim := range claimKeys {
+			c.Set(claim, mapClaims[claim])
+		}
 
-        c.Next()
-    }
+		c.Next()
+	}
 }
 
 func ErrorMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        c.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-        c.Next()
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+		c.Next()
 
-        httpError := new(errors.HTTPError)
-        httpError.Code = http.StatusInternalServerError
-        httpError.Message = "Internal Server Error"
-        httpError.Resolution = "Please try again later"
-        httpError.Meta = nil
-        errs := c.Errors
+		httpError := new(errors.HTTPError)
+		httpError.Code = http.StatusInternalServerError
+		httpError.Message = "Internal Server Error"
+		httpError.Resolution = "Please try again later"
+		httpError.Meta = nil
+		errs := c.Errors
 
-        if len(errs) > 0 {
-            err := errs[0].Err
-            httpError.Err = err.Error()
+		// if the response is already written internally by gin, return
+		if c.Writer.Written() {
+			return
+		}
 
-            appErr := new(errors.AppError)
-            validationErr := new(validator.ValidationErrors)
-            ginErr := new(gin.Error)
+		if len(errs) > 0 {
+			err := errs[0].Err
+			httpError.Err = err.Error()
 
-            switch {
-            case pkgerr.As(err, validationErr):
-                httpError.Message = validationErr.Error()
-                httpError.Code = http.StatusBadRequest
-                httpError.Message = "Bad Request"
-                httpError.Resolution = "Provide valid data and try again!"
-                c.JSON(httpError.Code, httpError)
-            case pkgerr.As(err, ginErr):
-                httpError.Message = ginErr.Error()
-                httpError.Meta = ginErr.Meta
-                c.JSON(httpError.Code, httpError)
-            case pkgerr.As(err, appErr):
-                httpError.Code = appErr.Code
-                httpError.Resolution = appErr.Resolution
-                httpError.Message = appErr.Message
-                httpError.Err = appErr.Err.Error()
+			appErr := new(errors.AppError)
+			validationErr := new(validator.ValidationErrors)
+			ginErr := new(gin.Error)
 
-                c.JSON(httpError.Code, httpError)
-            default:
-                c.JSON(http.StatusInternalServerError, httpError)
-            }
-        }
-    }
+			switch {
+			case pkgerr.As(err, validationErr):
+				httpError.Message = validationErr.Error()
+				httpError.Code = http.StatusBadRequest
+				httpError.Message = "Bad Request"
+				httpError.Resolution = "Provide valid data and try again!"
+				c.JSON(httpError.Code, httpError)
+			case pkgerr.As(err, ginErr):
+				httpError.Message = ginErr.Error()
+				httpError.Meta = ginErr.Meta
+				c.JSON(httpError.Code, httpError)
+			case pkgerr.As(err, appErr):
+				httpError.Code = appErr.Code
+				httpError.Resolution = appErr.Resolution
+				httpError.Message = appErr.Message
+				httpError.Err = appErr.Err.Error()
+
+				c.JSON(httpError.Code, httpError)
+			default:
+				c.JSON(http.StatusInternalServerError, httpError)
+			}
+		}
+	}
+}
+
+func BindingError(c *gin.Context, err error) {
+	c.AbortWithStatusJSON(c.Writer.Status(), errors.HTTPError{
+		Code:       c.Writer.Status(),
+		Message:    "invalid request body",
+		Err:        err.Error(),
+		Resolution: "please check request body and try again",
+		Meta:       nil,
+	})
+}
+
+func BasicAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		username, password, _ := c.Request.BasicAuth()
+
+		hashedUsername := sha512.Sum512([]byte(username))
+		hashedPassword := sha512.Sum512([]byte(password))
+
+		expectedUsername := sha512.Sum512([]byte(os.Getenv("AUTH_USERNAME")))
+		expectedPassword := sha512.Sum512([]byte(os.Getenv("AUTH_PASSWORD")))
+
+		u := bytes.Compare(hashedUsername[:], expectedUsername[:])
+		p := bytes.Compare(hashedPassword[:], expectedPassword[:])
+
+		if u == 0 && p == 0 {
+			c.Next()
+			return
+		}
+
+		c.AbortWithStatusJSON(http.StatusUnauthorized, errors.HTTPError{
+			Code:       http.StatusUnauthorized,
+			Message:    "unauthorized",
+			Err:        "unauthorized",
+			Resolution: "check username and password and try again",
+			Meta:       nil,
+		})
+	}
 }
