fa36cc644b0c654b226f6b45f802948fa84ca66c
Swapnil Nakade <46112765+swagftw@users.noreply.github.com>
2023-07-31 14:34:30 +0530

    feat: add refresh token generation and tests in jwt.go & jwt_test.go
    Added functionality to generate a refresh token in jwt.go. Also, included test cases for the same in jwt_test.go. The new GenerateRefreshToken function takes in an access token and secret as input and returns a signed refresh token. Functions and variables names were also refactored and code style was made consistent.

--- jwt.go
+++ jwt.go
@@ -1,24 +1,22 @@
 package utils
 
 import (
-	"crypto/sha512"
-	"encoding/hex"
 	"errors"
 	"fmt"
-	"strconv"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
+	jwtv5 "github.com/golang-jwt/jwt/v5"
 )
 
 var ErrGeneratingAccessToken = errors.New("error generating access token")
+var ErrGeneratingARefreshToken = errors.New("error generating refresh token")
 var ErrInvalidAccessToken = errors.New("invalid access token")
 
 // GenerateAccessToken generates access token with signing method HS512 and given claims.
 func GenerateAccessToken(ttl time.Duration, secret string, claims map[string]string) (string, error) {
 	exp := time.Now().Add(ttl).Unix()
 
-	jwtClaims := jwt.MapClaims{
+	jwtClaims := jwtv5.MapClaims{
 		"exp": exp,
 	}
 
@@ -27,7 +25,7 @@ func GenerateAccessToken(ttl time.Duration, secret string, claims map[string]str
 		jwtClaims[key] = value
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwtClaims)
+	token := jwtv5.NewWithClaims(jwtv5.SigningMethodHS512, jwtClaims)
 
 	signedToken, err := token.SignedString([]byte(secret))
 	if err != nil {
@@ -39,28 +37,30 @@ func GenerateAccessToken(ttl time.Duration, secret string, claims map[string]str
 
 // GenerateRefreshToken generates refresh token from access token string
 // it concatenates access token with current time and hashes it using sha512
-func GenerateRefreshToken(accessToken string) string {
-	// concatenate access token with current time
-	key := accessToken + strconv.Itoa(int(time.Now().UnixMicro()))
+func GenerateRefreshToken(accessToken, secret string) (string, error) {
+	token, err := ParseToken(accessToken, secret)
+	if err != nil {
+		return "", ErrInvalidAccessToken
+	}
 
-	// hash the key
-	h := sha512.New()
-	h.Write([]byte(key))
+	jwtv5.NewWithClaims(jwtv5.SigningMethodHS512, token.Claims)
 
-	// return hex encoded hash
-	hashString := hex.EncodeToString(h.Sum(nil))
+	signedToken, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", ErrGeneratingARefreshToken
+	}
 
-	return hashString
+	return signedToken, nil
 }
 
 // ParseToken parses the given token string using the given secret and returns the token object
-func ParseToken(accessToken, secret string) (*jwt.Token, error) {
-	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+func ParseToken(accessToken, secret string) (*jwtv5.Token, error) {
+	token, err := jwtv5.Parse(accessToken, func(token *jwtv5.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwtv5.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
-		return secret, nil
+		return []byte(secret), nil
 	})
 
 	if err != nil {
--- jwt_test.go
+++ jwt_test.go
@@ -35,3 +35,38 @@ func TestGenerateAccessToken(t *testing.T) {
 	// verify the claims
 	assert.Equal(t, claims["username"], parsedToken.Claims.(jwt.MapClaims)["username"])
 }
+
+func TestGenerateRefreshToken(t *testing.T) {
+	// define test case values
+	ttl := 2 * time.Minute
+	secret := "secret"
+	claims := map[string]string{"username": "testuser"}
+
+	// call the function
+	tokenString, err := GenerateAccessToken(ttl, secret, claims)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token, err := GenerateRefreshToken(tokenString, secret)
+
+	// Test case: Check if a token is returned with no error
+	t.Run("token creation success", func(t *testing.T) {
+		assert.NoError(t, err)
+		assert.NotEmpty(t, token)
+	})
+
+	// Test case: Check if error is returned when invalid access token is passed
+	t.Run("invalid access token", func(t *testing.T) {
+		_, err = GenerateRefreshToken("invalidToken", secret)
+		assert.Error(t, err)
+		assert.Equal(t, ErrInvalidAccessToken, err)
+	})
+
+	// Test case: Check if error is returned when invalid secret is passed
+	t.Run("invalid secret", func(t *testing.T) {
+		_, err = GenerateRefreshToken(tokenString, "invalidSecret")
+		assert.Error(t, err)
+		assert.Equal(t, ErrInvalidAccessToken, err)
+	})
+}
